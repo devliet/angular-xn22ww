08b75089d760c8b86c3276e9ec53a62a0aad9949
devliet <101347493+devliet@users.noreply.github.com>
2023-03-17 14:47:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-xn22ww
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-xn22ww)
---
+++ src/app/hero.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class HeroService {
+  private heroesUrl = 'api/heroes';
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
+
+  //getHeroes(): Observable<Hero[]> {
+  //  const heroes = of(HEROES);
+  // this.messageService.add('HeroService: fetched heroes');
+  // return heroes;
+  //}
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((_) => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap((_) => this.log('fetched hero id=${id}')),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  //getHero(id: number): Observable<Hero> {
+  // For now, assume that a hero with the specified `id` always exists.
+  // Error handling will be added in the next step of the tutorial.
+  //  const hero = HEROES.find((h) => h.id === id)!;
+  // this.messageService.add(`HeroService: fetched hero id=${id}`);
+  //  return of(hero);
+  //}
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
